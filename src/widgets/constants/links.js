28a0ac9893b1e32ce5640de8c7ecc548a269e73e
Qing Guo <guoqing1001@hotmail.com>
2018-06-30 17:01:09 -0400

    :sparkles: replaced the images, added text for projects

--- src/widgets/constants/links.js
+++ src/widgets/constants/links.js
@@ -17,26 +17,24 @@ export const links = [
     external_link: 'https://www.hearfulhub.com/',
     source: hearfulSM,
     lg_source: hearful,
-    // source: 'http://kyledecker.me/content/1-work/2-wintr-mx/preview.svg',
-    // lg_source: 'http://kyledecker.me/content/1-work/2-wintr-mx/banner.svg',
     overview: [
       {
         item: 'context',
-        desc: 'Design, develop, write copy, and manage affairs.',
+        desc: 'Moving fast, rapid-chanage requirements',
       },
       {
         item: 'my role',
         desc:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          'Lead the front end development, communication between the back end team and business team',
       },
       {
         item: 'technologies',
-        desc: 'Lorem, Ipsum, Dolor, Sit, Amet.',
+        desc: 'React for POC, Angular for MVP, Serveless, Kanban agile methodology',
       },
       {
         item: 'the challenge',
         desc:
-          'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+          'The reality of wide difference of plea process between the jurisdictions in US forces us to put more effort on designing an one-size-fits-all hierarchy in the front end, meaningwhile keep it as simple and fast as possible. There is always a trade-off between them, and things are getting even harder since requirements change rapidly',
       },
     ],
     detail: [
@@ -55,38 +53,44 @@ export const links = [
     desc: 'The Automated Adjudication Service',
     link: '/dashboard/projects/youplea',
     external_link: 'https://youplea.com/',
-    // source: 'http://kyledecker.me/content/1-work/1-swiftype/preview.svg',
-    // lg_source: 'http://kyledecker.me/content/1-work/1-swiftype/banner.svg',
     source: ypSM,
     lg_source: yp,
     backgroundColor: '#6dc6b1',
     overview: [
       {
         item: 'context',
-        desc: 'Design, develop, write copy, and manage affairs.',
+        desc: 'Moving fast, rapid-chanage requirements.',
       },
       {
         item: 'my role',
         desc:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+          'Lead the front end development, communication between the back end team and business team.',
       },
       {
         item: 'technologies',
-        desc: 'Lorem, Ipsum, Dolor, Sit, Amet.',
+        desc: 'React for POC, Angular for MVP, Serveless, Kanban agile methodology.',
       },
       {
         item: 'the challenge',
         desc:
-          'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+          'Jurisdictions don’t all operate the same way and they serve different communities with different caseloads and through different processes. The reality forces us to put more effort on designing an one-size-fits-all hierarchy in the front end, to meet the current and potential clients needs meaningwhile keep it as simple as possible. There is always a trade-off between them, and things are getting even harder since requirements change rapidly.',
       },
     ],
     detail: [
       {
-        item: 'Lorem a ipsum',
+        item: 'Introduction',
         desc: [
-          'Proin nibh elit, consectetur eu ullamcorper nec, luctus eget augue. Donec nec elit nisi. Donec a placerat massa. Suspendisse pretium vitae libero at eleifend. Sed eu ante velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce placerat tristique mollis. Donec vitae ullamcorper dolor. Duis faucibus ac sapien vitae elementum. Donec posuere lacinia consectetur. Vestibulum interdum mollis purus, sed imperdiet odio dictum sed. Duis convallis arcu id purus suscipit commodo. Donec mi nunc, euismod pharetra tempus a, venenatis id dolor. Pellentesque eleifend, enim vel vehicula suscipit, eros lectus vestibulum erat, vitae pharetra ligula mauris eu justo.',
-          'Proin nibh elit, consectetur eu ullamcorper nec, luctus eget augue. Donec nec elit nisi. Donec a placerat massa.',
+          'Youplea helps issue plea agreements and diversion contracts more efficiently and provides valuable analytics that can improve the process.',
+          'It is designed for cases with clear criteria for a pretrial resolution, think criminal traffic, drug and alcohol offenses, and other low-level misdemeanors.',
         ],
+      },
+      {
+        item: 'What did I do',
+        desc: [],
+      },
+      {
+        item: 'Result',
+        desc: [],
       },
     ],
   },
@@ -98,8 +102,6 @@ export const links = [
     external_link: 'https://fanalytical.com/',
     source: fanSM,
     lg_source: fan,
-    // source: 'http://kyledecker.me/content/1-work/3-catnap/preview.svg',
-    // lg_source: 'http://kyledecker.me/content/1-work/3-catnap/banner.svg',
     backgroundColor: '#ffd221',
     overview: [
       {
@@ -139,8 +141,6 @@ export const links = [
     external_link: 'https://taxonomygui.herokuapp.com/',
     source: guiSM,
     lg_source: gui,
-    // source: 'http://kyledecker.me/content/1-work/4-misc/preview.svg',
-    // lg_source: 'http://kyledecker.me/content/1-work/4-misc/banner.svg',
     backgroundColor: '#63aaff',
     overview: [
       {
