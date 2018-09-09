fca4ccd16961f2bfd879ba0ae00f11087b518904
Qing Guo <guoqing1001@hotmail.com>
2018-09-09 18:41:42 -0400

    fix UI issue on the mobile sidebar, fix the position of spinner

--- src/widgets/loading.js
+++ src/widgets/loading.js
@@ -2,8 +2,11 @@ import React from "react";
 // import PropTypes from 'prop-types';
 import { Spin } from "antd";
 
+import ScrollToTopOnMount from "./scrollToTopOnMount";
+
 const Loading = () => (
   <div style={{ textAlign: "center", marginTop: "30vh" }}>
+    <ScrollToTopOnMount />
     <Spin tip="Loading ..." size="large" />
   </div>
 );
